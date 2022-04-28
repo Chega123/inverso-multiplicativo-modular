c476566409f40d3aa0231d3dbc9337b0b1d763ef
Chega123 <85765351+Chega123@users.noreply.github.com>
2022-04-27 23:11:58 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int EuclidesExtendido(int a, int b, int &x, int &y) {
+    if(b == 0) {
+       x = 1;
+       y = 0;
+       return a;
+    }
+    int x1, y1;
+    int euclides = EuclidesExtendido(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return euclides;
+}
+int euclides(int a,int b){
+  while(b!=0){
+    int aux=b;
+    b=a%b;
+    a=aux;
+  }
+  return a;
+}
+
+void inverso(int a, int n)
+{
+    int x, y;
+    int eucli = EuclidesExtendido(a, n, x, y);
+    if (eucli == 1){
+      cout<<x<<endl;
+      int resultado = (x % n + n) % n;
+      cout << "inverso es:  " << resultado;
+    }
+    else cout << "no existe";  
+}
+
+int main()
+{
+   int a, n, aux;
+  cout<<"insertar primer numero: ";
+    cin>>a;
+  cout<<"insertar segundo numero: ";
+    cin>>n;
+  if(n<0){cout<<"no cumple lo de n enteros positivos";}  
+  if ((a<0)&& (n<0)) {cout<< "Ingrese numeros positivos";}
+  else inverso(a,n);
+    
+   return 0;
+}
+
